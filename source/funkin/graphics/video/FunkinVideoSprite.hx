5fce946974294dbadd68e3e0e8e096838c6f5201
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-10-02 23:38:58 +0300

    Update FunkinVideoSprite.hx

--- source/funkin/graphics/video/FunkinVideoSprite.hx
+++ source/funkin/graphics/video/FunkinVideoSprite.hx
@@ -9,26 +9,14 @@ import hxvlc.flixel.FlxVideoSprite;
  */
 class FunkinVideoSprite extends FlxVideoSprite
 {
-  public var volume(default, set):Float = 1;
-
   public function new(x:Float = 0, y:Float = 0)
   {
     super(x, y);
 
-    set_volume(1);
-  }
-
-  override public function update(elapsed:Float):Void
-  {
-    super.update(elapsed);
-    set_volume(volume);
-  }
-
-  function set_volume(value:Float):Float
-  {
-    volume = value;
-    bitmap.volume = Std.int((FlxG.sound.muted ? 0 : 1) * (FlxG.sound.logToLinear(FlxG.sound.volume) * 100) * volume);
-    return volume;
+    getCalculatedVolume = function():Float
+    {
+      return (FlxG.sound.muted ? 0 : 1) * FlxG.sound.logToLinear(FlxG.sound.volume);
+    }
   }
 }
 #end
